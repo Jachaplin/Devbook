5677ba99672a339298103b377f0291324f3c86be
Jachaplin <jakealanchaplin@yahoo.com>
2018-11-11 16:52:17 -0800

    delete education and experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -189,6 +189,7 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
   })
 })
 
+
 // Route: POST api/profile/education
 // Desc: Add education to profile
 // Access: Private
@@ -219,6 +220,54 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
       .save()
       .then(profile => res.json(profile)); 
   })
+})
+
+// Route: DELETE api/profile/experience/:exp_id
+// Desc: Delete experience from profile
+// Access: Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+    // Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    // Splice out of array
+    profile.experience.splice(removeIndex, 1);
+
+    // Save
+    profile
+      .save()
+      .then(profile => res.json(profile))
+  })
+  .catch(err => res.status(404).json(err));
+})
+
+// Route: DELETE api/profile/education/:exp_id
+// Desc: Delete experience from profile
+// Access: Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+    // Get remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    // Splice out of array
+    profile.education.splice(removeIndex, 1);
+
+    // Save
+    profile
+      .save()
+      .then(profile => res.json(profile))
+  })
+  .catch(err => res.status(404).json(err));
 })
 
 module.exports = router;
