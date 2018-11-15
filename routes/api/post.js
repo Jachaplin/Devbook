c65be1c72c19c240c3b9d99d8ac595ad438edacd
Jachaplin <jakealanchaplin@yahoo.com>
2018-11-15 04:32:12 -0800

    delete add comments

--- routes/api/post.js
+++ routes/api/post.js
@@ -183,5 +183,4 @@ router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session
     .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
 })
 
-
 module.exports = router;
