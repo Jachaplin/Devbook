bc0342ddbfda0a58c0de97372ce360a6ed15267c
Jachaplin <jakealanchaplin@yahoo.com>
2018-11-18 20:06:38 -0800

    register and login templates

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Navbar from './components/layout/component_Navbar'
-import Footer from './components/layout/component_Footer'
-import Landing from './components/layout/component_Landing'
-import Register from './components/auth/component_Register'
-import Login from './components/auth/component_Login'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import './App.css';
 
 class App extends Component {
@@ -13,11 +13,11 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
-            <Route exact path="/" component={ Landing } />
-            <div className="container">
-              <Route exact path="/register" component={ Register } />
-              <Route exact path="/login" component={ Login } />
-            </div>
+          <Route exact path="/" component={ Landing } />
+          <div className="container">
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </div>
           <Footer />
         </div>
       </Router>
