a5bd3ef20ebbc6a4eb547ccf735f1052d4f0b71e
Jachaplin <jakealanchaplin@yahoo.com>
2018-12-01 04:17:33 -0800

    edit profile

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -5,7 +5,7 @@ const ProfileActions = () => {
   return (
     <div>
       <div className="btn-group mb-4" role="group">
-        <Link to="edit/profile" className="btn btn-light">
+        <Link to="edit-profile" className="btn btn-light">
           <i className="fas fa-user-circle text-info mr-1"></i>
           Edit Profile
         </Link>
